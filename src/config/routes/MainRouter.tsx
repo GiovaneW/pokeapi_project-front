129fc1c584232af767d5cd1bfdeb45d8868c3f87
Giovane Welter <giovane@spacecom.com.br>
2023-03-17 17:56:48 -0300

    feat: add pokemonFrom to router

--- src/config/routes/MainRouter.tsx
+++ src/config/routes/MainRouter.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider, NonIndexRouteObject, Routes, Route } from 'react-router-dom'
 import { ErrorBoundary } from '../../components/ErrorBoundary'
-import { home, pokemonList, testComponent } from '../../views'
+import { home, pokemonForm, pokemonList, testComponent } from '../../views'
+import PokemonForm from '../../views/pokemon/PokemonForm'
 
 export interface IRouteProps extends NonIndexRouteObject {
     breadcrumb?: string
@@ -24,14 +25,14 @@ export function MainRoutes(props: { routes: Array<IRouteProps> }): React.ReactEl
     return (
         <Routes>
             <Route path='/' element={home} />
-            <Route path='test' element={home}>
-                <Route path=':id' element={home} />
-            </Route>
-            <Route path='pokemons' element={pokemonList}>
-                <Route path=':id' element={testComponent}>
-                    <Route path='edit' element={<></>} />
-                </Route>
-            </Route>
+            <Route path='test' element={home} />
+            <Route path='/pokemons' element={pokemonList} />
+            <Route path='/pokemons/:id' element={testComponent} />
+            {/* <Route path='/pokemons/:id/edit' element={pokemonForm}  /> */}
+            <Route path='/pokemons/:id/edit' element={testComponent} loader={(loader) => {
+                console.log(loader)
+            }} />
+            <Route path='/pokemons/create' element={pokemonForm} />
             <Route path='*' element={<ErrorBoundary />} />
         </Routes>
     )
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,9 +2,11 @@ import React, { lazy, Suspense } from 'react'
 
 const Home = lazy(() => import('./home/Home')),
     Test = lazy(() => import('./Test')),
-    PokemonList = lazy(() => import('./pokemon/PokemonList'))
+    PokemonList = lazy(() => import('./pokemon/PokemonList')),
+    PokemonForm = lazy(() => import('./pokemon/PokemonForm'))
 
 export const
     home = <Suspense ><Home /></Suspense>,
     testComponent = <Suspense ><Test /></Suspense>,
-    pokemonList = <Suspense ><PokemonList /></Suspense>
+    pokemonList = <Suspense ><PokemonList /></Suspense>,
+    pokemonForm = <Suspense ><PokemonForm /></Suspense>
